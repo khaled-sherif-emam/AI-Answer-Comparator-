b13ac1f8232e80a688a5f4463e38e1b2d295a53b
Khaled Emam <khaled.memam05@gmail.com>
2025-09-27 22:51:27 +0100

    Vercel publish attempt 1

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5002';
+export const API_BASE_URL = process.env.VITE_REACT_APP_API_BASE_URL;
 
 export const API_ENDPOINTS = {
   AUTH: {
